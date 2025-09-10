8a783bfca9794e386b9fb0d5f45611c80caad272
Mx-Iris <mxiris@126.com>
2025-09-11 00:31:25 +0800

    Fix Xcode 26 build errors

--- Package.swift
+++ Package.swift
@@ -1,15 +1,16 @@
-// swift-tools-version:5.1
+// swift-tools-version: 6.1
 import PackageDescription
 
 let package = Package(
     name: "CombineCocoa",
-    platforms: [.iOS(.v10), .macOS(.v10_15)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(name: "CombineCocoa", targets: ["CombineCocoa"]),
     ],
     dependencies: [],
     targets: [
-        .target(name: "CombineCocoa", dependencies: ["Runtime"]),
-        .target(name: "Runtime", dependencies: [])
-    ]
+        .target(name: "CombineCocoa", dependencies: ["CombineCocoaRuntime"]),
+        .target(name: "CombineCocoaRuntime", dependencies: []),
+    ],
+    swiftLanguageModes: [.v5]
 )
